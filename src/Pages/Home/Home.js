f1f30467c974fa58b4e79faaed640aaea4d0f645
Raju1-Ahmed <robiussaniraju@gmail.com>
2022-07-02 21:23:07 +0600

    category is almost done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Category from './Category';
+import ProductSlick from './ProductSlick';
 
 const Home = () => {
     return (
         <div>
             <Category></Category>
+            <ProductSlick></ProductSlick>
         </div>
     );
 };
